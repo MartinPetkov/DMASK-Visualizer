cde085f12b864ae38da66d12891771ec537352cf
Elaine Huynh <elaine.huynh@mail.utoronto.ca>
2015-06-02 15:58:28 -0400

    Added front end code (work in progress) -- I'll organize things more later.

---
+++ front-end-code/loader.js
@@ -0,0 +1,60 @@
+var toggled = 0;
+
+function updateInputHeight(maxheight){
+    // Return the height for the input tables (50% if there are 2, 100% otherwise)
+    var numberOfTables = $('.input').length;
+    if (numberOfTables > 1){
+        return (maxheight / 2);
+    }
+    return maxheight;
+}
+
+function updateHeight(){
+    // Return the new height of the table display area (accounting for the storage toggle)
+    var newheight;
+    if (!toggled){
+        newheight = $("#workspace").height() - $("#namebox").height() - $("#inventoryheader").height();
+    } else {
+        newheight = $("#workspace").height() - $("#namebox").height() - $("#inventory").height();
+    }
+    return newheight;
+}
+
+function toggleInventory(){
+    // Toggle the inventory's appearance
+    toggled = 1 - toggled;
+    var newheight = updateHeight();
+    $("#tablespace").animate({height: newheight});
+    $('.input').animate({height: updateInputHeight(newheight)});
+}
+
+function hoverText(newtext){
+    // Adjusts the hovertext display
+    $('#namebox').text(newtext);
+}
+
+//Initial load of page
+$(document).ready(onPageLoad);
+
+//Every resize of window
+$(window).resize(sizeContent);
+
+function onPageLoad(){
+    sizeContent();
+}
+
+//Dynamically assign height
+function sizeContent() {
+    var newHeight = $("html").height() - $("#header").height() - $("#footer").height() + "px";
+    $("#content").css("height", newHeight);
+    
+    // Update the height of the Table of Contents
+    var newheight = $("#leftbar").height() - $("#navbar").height();
+    $("#toc").height(newheight);
+
+    // Update the height of the table display area
+    $("#tablespace").height(updateHeight());
+    var tableheight = $("#tablespace").height();
+    $('.input').height(updateInputHeight(tableheight));
+}
+
