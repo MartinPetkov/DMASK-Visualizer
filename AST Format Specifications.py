570a26359c01cb74aa46f5c8bbe31d51a7c552f1
gekath <kathyge@gmail.com>
2015-07-30 09:49:03 -0400

    AST specifications renamed

---
+++ AST Format Specifications.py
@@ -0,0 +1,86 @@
+"""
+LEGEND:
+    <sql_query> = symbol built based on rules defined elsewhere in the document
+    "SELECT"    = the literal word SELECT
+    'col_name'  = any string representing the column name
+    [<arg>]     = a list containing one example of <arg>
+    [<arg> ...] = a list containing any number of <arg>s
+    (<elem>?)   = optional element
+    (<elem>)+   = 1 or more of this element
+    (<elem>)*   = 0 or more of this element
+"""
+
+<AST>               = <sql_query> | <create_view> | [ <sql_query> (<set_operation> <sql_query>)+ ]
+
+<set_operation>     = "UNION" | "INTERSECT" | "EXCEPT"
+
+<sql_query>         = [ <sql_statement> ... ]
+
+<sql_statement>     =       [ "SELECT",     [ <select_arg> ... ]],
+                            [ "FROM",       [ <from_arg>, (<from_connector>?) ... ] ],
+                            ([ "WHERE",     [ <where_arg> (<where_connector>?) ... ] ])?,
+                            ([ "GROUP BY",  [ 'col_name' ... ] ])?,
+                            ([ "HAVING",    [ <aggregate_fn>, <comparator>, <val> ]])?,
+                            ([ "ORDER BY",  [ 'col_name' ... ] ])?,
+                            ([ "LIMIT",     [ <numeric>] ] )?,
+                            ([ "OFFSET",    [ <numeric>] ] )?
+
+<create_view>       = [ "CREATE VIEW", ("AS"?), 'view_name', <sql_query> ]
+
+
+# For SELECT
+<select_arg>        = ['col_name']
+                        |   [ ('col_name'
+                            | <col_equation>
+                            | <aggregate_fn>
+                            | <val> ), <as>, 'new_name']
+
+
+<col_equation>      = ['col_name', (<operator>, 'col_name')+ ]
+
+<aggregate_fn>      = function('col_name')
+
+<operator>          = "+" | "-" | "*" | "/" | "||"
+
+<as>                = " " | "AS"
+
+
+# For FROM
+<from_arg>          = ['table_name']
+                        |   [ ('table_name' | <sql_query>), <as>, 'new_name']
+                        |   [ 'table_name', 'ON', <reason> ]
+
+<from_connector>    = ","
+                        | ("NATURAL"
+                            | "INNER"
+                            | ("LEFT" | "RIGHT" | "FULL")? "OUTER"
+                            | "CROSS"
+                            | "LEFT"
+                            | "RIGHT"
+                            | "FULL"
+                        )? "JOIN"
+
+
+# For WHERE
+<where_arg>         = [("NOT", ?) <reason> ] | [<where_arg>]
+
+<where_connector>   = "AND" | "OR"
+
+<reason>            = 'col_name', <comparator>, <val>
+                        | 'col_name', "IN", <val>
+                        | 'col_name', ("IN" | (<comparator>, ("ANY" | "ALL"))), <sql_query>
+                        | "EXISTS", <sql_query>
+                        | 'col_name', (("IS", ("NULL" | "NOTNULL")) | "ISNULL")
+                        | 'col_name', ("NOT"),? "BETWEEN", <val>, "AND", <val>
+
+
+<as>                = " " | "AS"
+
+<comparator>        = "=" | "<>" | "!=" | ">" | ">=" | "<" | "<="
+
+<numeric>           = /^[1-9][0-9]*$/
+
+<val>               = <numeric> | 'quoted string'
+
+
+
