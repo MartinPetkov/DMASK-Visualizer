5df2f12f48dd1cf3a0457cee1da353a2c26a6ee1
sophia-huynh <2575.00f@gmail.com>
2015-08-01 18:45:11 -0400

    Finished Tables/Reasons. The only things that need to be done are checking the query executions and maybe? handling attributes like count(avg). I need to get Postgres fixed on my computer first, though.

--- dmask.py
+++ dmask.py
@@ -12,8 +12,8 @@ from parsed_query import *
 from query_step import *
 from table import *
 
-import sql_parser
-import ra_parser
+#import sql_parser
+#import ra_parser
 import copy
 
 
@@ -130,8 +130,11 @@ class DMASK:
 def get_all_conditions(sql_chunk):
     # Get the AST for the WHERE clause
     ast = sql_parser.sql_to_ast(sql_chunk)[1:]
+    # Given an AST for the WHERE clause, return a tuple containing 2 items:
+    # The first is a list of all the conditions in the where clause (ex. ["grade >= 80", "cnum in SELECT ..."])
+    # The second is a dictionary mapping a condition to its AST {"condition": [AST]}
     return conditions_helper(ast)
-    
+
 def conditions_helper(ast):
     # If the first item in a list is a string, then that list contains a
     # condition -- Traverse the AST and return all conditions
@@ -158,10 +161,17 @@ def find_subquery(ast):
             return item    
     
 def get_reasons(conditions, subqueries, input_step, dmask):
+    # Given a list of conditions, subqueries, the input step and dmask object,
+    # return the Reasons
+    
     input_query = input_step.executable_sql
     input_table = input_step.result_table
     input_tuples = input_table.tuples
     reasons = {0:Reason([])}
+    
+    # Get the namespace of the entire query (only really need the FROM clause
+    # of the original query)
+    namespace = get_namespace(sql_parser.sql_to_ast(input_query), dmask)
     
     connection = psycopg2.connect(dmask.conn_params)
     cursor = connection.cursor()
@@ -206,16 +216,22 @@ def get_reasons(conditions, subqueries, input_step, dmask):
             # Substitute and execute the correlated subquery
             if correlated:
                 # Get the items to substitute
-                # Depends on: substituted_query.substitutable
-                #             the row itself (and your columns)
-                #             parent table (for aggregate functions)
-                # ---- Create namespace for the current table (function used in get_correlated_elements?)
-                # ---- Go through and add those to the dictionary for substitutions
-                # ---- If something does not appear in the namespace (ex. "count(oid)"), run it through the parent
-                # -------- table (SELECT <that item> FROM <from clause>)
+                substitutes = {}
+                
+                for item in correlated:
+                    for j in range(len(columns)):
+                        if matches_alias(namespace, columns[j], item):
+                            substitutes[item] = tuples[i][j]
+                            break
+                    # TODO:
+                    # If substitutes[item] does not exist at this point, then
+                    # it might be an aggregate function, in which case
+                    # you will want to do "SELECT item FROM <from clause>"
+                    # which may or may not fail
+                
                 
                 # Substitute them in the query
-                substituted_query = pq.substitute()
+                substituted_query = pq.substitute(substitutes)
                 
                 # Create the parsed query
                 steps = sql_parser.sql_ast_to_steps(substituted_subquery, dmask.base_tables)
@@ -237,28 +253,136 @@ def get_reasons(conditions, subqueries, input_step, dmask):
                 if correlated:
                     reasons[i+1].subqueries[condition] = parsed_query
                     if kept:
-                        reasons[i+1].passed_subqueries.append(condition)
-                
-                
-def get_correlated_elements(subquery):
-    # subquery is an AST representing the subquery
-    # get all attributes (aka anything that is not a keyword)
-    # and the table names that are brought in (via the FROM statement)
-    # get the namespaces for those tables
-    # remove any elements that are not in the namespace
-    # return the list of remaining attributes
-    # --> if it's empty, then the query is uncorrelated
-    
-    
-    # ---- Get a list of all attributes that are called upon in the subquery (ex. 'oid')
-    # ---- Get the subquery's namespace (requires "SELECT * FROM <from clause of subquery>" and getting the column names + prefixing things + keeping track of all the tables in the FROM clause)
-    # ---- For every attribute, if it appears in the namespace, remove it.
-    # ---- If the list is empty at the end, it's uncorrelated.    
-    pass
-    
+                        reasons[i+1].passed_subqueries.append(condition)      
+    return reasons
 
-def get_namespace():
-    pass
+
+def get_correlated_elements(query):
+    # Given an AST representing a query, return a list of elements that do not
+    # appear in the namespace of the (isolated) query.
+    
+    # get the namespace of the subquery
+    namespace = flatten_list(get_namespace(subquery))
+    
+    # get the attributes called in the subquery
+    attributes = find_attributes(subquery)
+    
+    # for each attribute, if it appears somewhere in the namespace, remove it
+    i = 0
+    while (i < len(attributes)):
+        if attributes[i] in namespace:
+            attributes.pop(i)
+        else:
+            i += 1
+    
+    # return the list of remaining attributes (which did not appear in the namespace)
+    return attributes
+
+def find_attributes(query):
+    # Given an AST representing a query, return a list of the attributes that appear
+    # in the query (anything that is not a keyword or table name is an attribute) 
+    
+    # DOES NOT HANDLE SUBQUERIES -- Subqueries would need to be handled recursively
+    query = query[:]
+    
+    # Remove the FROM clause
+    for i in range(len(query)):
+        if query[i][0].lower() == "from":
+            query.pop(i)
+            break
+    
+    attributes = remove_keywords(flatten_list(query))
+    return attributes
+
+def remove_keywords(query):
+    # Goes through a list and removes any keywords
+    i = 0
+    while i < len(query):
+        if (is_keyword(query[i])):
+            query.pop(i)
+        else:
+            i += 1
+    return query
+
+def is_keyword(string):
+    punctuation = "~!@#$%^&*()-_=+[]\\|/.,<> \n"
+    # At the moment, this list is nonexhaustive (see
+    # http://www.postgresql.org/docs/9.3/static/sql-keywords-appendix.html
+    # for full list of keywords)
+    KEYWORDS = ["CREATE VIEW", "FROM", "WHERE", "HAVING", "GROUP BY", "SELECT",
+                "DISTINCT", "UNION", "ORDER BY", "LIMIT", "OFFSET", "OR", "AND",
+                "ANY", "ALL", "AS", "ASC", "DESC", "AVG", "BOTH", "ALWAYS",
+                "BETWEEN", "JOIN", "NATURAL JOIN", ",", "EXCEPT", "EQUALS",
+                "IN", "MIN", "MAX", "COUNT", "LEFT JOIN", "RIGHT JOIN",
+                "LEFT INNER JOIN", "LEFT OUTER JOIN", "RIGHT INNER JOIN",
+                "RIGHT OUTER JOIN"]
+
+    # If subqueries are handled recursively, then none of the 'from clause' keywords
+    # has to be mentioned
+    
+    string = string.upper().strip(punctuation)
+    if not string or string.isnumeric() or string[0] == '"' or string[0] == "'":
+        return True
+    return (string in KEYWORDS)
+
+def get_namespace(subquery, dmask):
+    # Given an AST, return the namespace of the table in the form:
+    # [[column, prefix.column, ...], [column, prefix.column, ...]]
+    
+    from_clause = []
+    
+    for node in subquery:
+        if node[0].lower() == "from":
+            from_clause = node[1]
+            break
+    
+    on_using = False
+    
+    # tables is a list of (name, alias) tuples
+    tables = []
+    
+    # Get all of the tables brought in (ex. FROM Took t, Student -> [(Took, t), (Student, Student)])
+    for item in from_clause:
+        if isinstance(item, list):
+            if not on_using:
+                # If it's a list, then the elements inside is a table brought in
+                # (except if preceeded by ON/USING)
+                name = item[0]
+                alias = item[-1]
+                tables.append((name, alias))
+            on_using = False
+        elif item.lower() in ["on", "using"]:
+            on_using = True
+    
+    main_table = " ".join(flatten_list(from_clause))
+    namespace = "SELECT * FROM " + main_table # TODO: execute this + get the columns (store in namespace as [column], [column]
+    
+    # Traverse the list of tables, adding all of the columns to the namespace
+    for table in tables:
+        columns = "SELECT * FROM " + table[0] # TODO: execute this + get the columns
+        prefix = table[1]
+        for column in columns:
+            for name in namespace:
+                if column == name[0]:
+                    name.append(prefix + "." + column)
+                    break
+
+    return namespace
+
+def matches_alias(namespace, attribute, to_match):
+    # Given a namespace, attribute (ex. "sid") and someting to match (ex. "Took.sid")
+    # Returns true whether the attribute matches to_match
+    
+    if attribute == to_match:
+        return True
+    
+    for item in namespace:
+        if to_match in item:
+            return True
+    
+    return False
+    
+    
 
 def get_table_name(exsqltable):
     # Given an executable SQL statement, return the table's name
@@ -269,7 +393,7 @@ def get_table_name(exsqltable):
     
     # Traverse the AST until the FROM clause is found
     for node in ast:
-        if node[0] == "FROM":
+        if node[0].lower() == "FROM":
             # The second element in the node [FROM, [...]] holds the table's name
             from_ast = node[1]
             name = []
